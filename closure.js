3b065d716fe3abdd4802d8ad58c06f4fb3958f53
Jarrod Jack <jarrodjack@Jarrods-MacBook-Pro.local>
2023-10-27 13:46:32 -0400

    closure challenge 10

--- closure.js
+++ closure.js
@@ -47,3 +47,35 @@ function objOfMatches(array1, array2, callback) {
 
 console.log(objOfMatches(['hi', 'howdy', 'bye', 'later', 'hello'], ['HI', 'Howdy', 'BYE', 'LATER', 'hello'], checkIfHasMatching));
 // should log: { hi: 'HI', bye: 'BYE', later: 'LATER' }
+
+
+
+// Challenge 10
+// Construct a function multiMap that will accept two arrays: an array of values and an array of callbacks. multiMap will return an object whose keys match the elements in the array of values. The corresponding values that are assigned to the keys will be arrays consisting of outputs from the array of callbacks, where the input to each callback is the key.
+function multiMap(arrVals, arrCallbacks) {
+
+  let obj = {}
+
+  for(let i = 0; i < arrVals.length; i++) {
+    obj[arrVals[i]] = []
+    for(let j = 0; j < arrCallbacks.length; j++) {
+      obj[arrVals[i]].push(arrCallbacks[j](arrVals[i]))
+    }
+  }
+  return obj
+}
+
+console.log(multiMap(
+  ['catfood', 'glue', 'beer'],
+  [
+    function(str) { return str.toUpperCase(); },
+    function(str) { return str[0].toUpperCase() + str.slice(1).toLowerCase(); },
+    function(str) { return str + str; }
+  ]));
+/* should log: 
+ { 
+ catfood: ['CATFOOD', 'Catfood', 'catfoodcatfood'], 
+ glue: ['GLUE', 'Glue', 'glueglue'], 
+ beer: ['BEER', 'Beer', 'beerbeer'] 
+ }
+ */
